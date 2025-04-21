7b5d8b4fb2c2d7f8780e34eeccd3b3db5a885097
Vishal Kaviyarasu <vishal@example.com>
2025-04-21 12:57:08 +0530

    Updated Notification model

--- backend/models/notificationModel.js
+++ backend/models/notificationModel.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const notificationSchema = new mongoose.Schema({
   complaintID: {
-    type: Number,
+    type: String,
     required: true,
     ref: "Complaint",
   },
