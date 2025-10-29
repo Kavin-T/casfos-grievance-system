12691092e56ecb1cecba3fc0e763efb35b0fcf72
Kavin-T <kavin14edu@gmail.com>
2025-10-29 19:58:03 +0530

    Fix for changing API Base URL Dynamically (to support access from lan)

--- frontend/src/services/axios.js
+++ frontend/src/services/axios.js
@@ -20,7 +20,18 @@
 
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_BACKEND_API_URL;
+const hostname = window.location.hostname;
+
+let BASE_URL = process.env.REACT_APP_BACKEND_API_URL_PROD; // default
+let DOMAIN = process.env.REACT_APP_DOMAIN_PROD; // default
+
+if (hostname === "localhost") {
+  BASE_URL = process.env.REACT_APP_BACKEND_API_URL_DEV;
+} else if (hostname === DOMAIN) {
+  BASE_URL = process.env.REACT_APP_BACKEND_API_URL_PROD;
+} else {
+  BASE_URL = process.env.REACT_APP_BACKEND_API_URL_LOCAL;
+}
 
 axios.defaults.baseURL = BASE_URL;
 axios.defaults.withCredentials = true;
